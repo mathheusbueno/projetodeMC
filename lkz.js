b6e5664a94df742f27308551a519e72bb67a994e
mathheusbueno <mathheusbueno@gmail.com>
2018-10-14 00:56:34 -0300

    Create lkz.js

---
+++ lkz.js
@@ -0,0 +1,62 @@
+var ctx = document.getElementById('myChart').getContext("2d");
+var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+gradientStroke.addColorStop(0, '#80b6f4');
+gradientStroke.addColorStop(1, '#f49080');
+
+var gradientFill = ctx.createLinearGradient(500, 0, 100, 0);
+gradientFill.addColorStop(0, "rgba(128, 182, 244, 0.6)");
+gradientFill.addColorStop(1, "rgba(244, 144, 128, 0.6)");
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"],
+        datasets: [{
+            label: "Temperatura ºC",
+            borderColor: gradientStroke,
+            pointBorderColor: gradientStroke,
+            pointBackgroundColor: gradientStroke,
+           pointHoverBackgroundColor: gradientStroke,
+            pointHoverBorderColor: gradientStroke,
+            pointBorderWidth: 10,
+            pointHoverRadius: 10,
+            pointHoverBorderWidth: 1,
+            pointRadius: 3,
+            fill: true,
+            backgroundColor: gradientFill,
+            borderWidth: 4,
+            data: [js_array[0], js_array[1], js_array[2], js_array[3], js_array[4], js_array[5], js_array[6], js_array[7], js_array[8], js_array[9], js_array[10], js_array[11], js_array[12], js_array[13], js_array[14], js_array[15], js_array[16], js_array[17]]
+}]
+    },
+    options: {
+        legend: {
+            position: "bottom"
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold",
+                    beginAtZero: true,
+                    maxTicksLimit: 5,
+                    padding: 20
+                },
+                gridLines: {
+                    drawTicks: false,
+                    display: false
+                }
+
+            }],
+            xAxes: [{
+                gridLines: {
+                    zeroLineColor: "transparent"
+                },
+                ticks: {
+                    padding: 20,
+                    fontColor: "rgba(0,0,0,0.5)",
+                    fontStyle: "bold"
+                }
+            }]
+        }
+    }
+});
