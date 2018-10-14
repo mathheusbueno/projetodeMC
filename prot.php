286c56a8bb342f88fd9a312e74eced47333de245
mathheusbueno <mathheusbueno@gmail.com>
2018-10-14 00:32:34 -0300

    Create prot.php

---
+++ prot.php
@@ -0,0 +1,7 @@
+<?php
+// pagina protegida, incluir script de verificação de login
+require 'verificarLogin.php';
+?>
+ 
+<h1>P&aacute;gina protegida!</h1>
+<p>Ol&aacute; <u><?php echo $_SESSION['username']; ?></u>, esta &eacute; a p&aacute;gina protegida</p>
